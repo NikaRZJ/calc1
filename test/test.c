aa1e85190557aeea59d911ecfe91046484b7e310
Ubuntu <nikar_50@mail.ru>
2018-09-11 04:45:47 -0700

    update tests

--- test/test.c
+++ test/test.c
@@ -73,3 +73,75 @@ CTEST(menu, check_Subtraction)
 	ASSERT_EQUAL(exp3, result3);
 	ASSERT_EQUAL(exp4, result4);
 }
+
+CTEST(menu, check_multiplication)
+{
+	//Given
+	char str1[] = "*";
+	char str2[] = "vgfo";
+	char str3[] = "*frvirejvire";
+	char str4[] = "+";
+	//When
+	const int result1 = CheckMultiplication(str1);
+	const int result2 = CheckMultiplication(str2);
+	const int result3= CheckMultiplication(str3);
+	const int result4= CheckMultiplication(str4);
+	//Then
+	const int exp1 = 1;
+	const int exp2 = 0;
+	const int exp3 = -1;
+	const int exp4 = 0;
+
+	ASSERT_EQUAL(exp1, result1);
+	ASSERT_EQUAL(exp2, result2);
+	ASSERT_EQUAL(exp3, result3);
+	ASSERT_EQUAL(exp4, result4);
+}
+
+CTEST(menu, check_division)
+{
+	//Given
+	char str1[] = "/";
+	char str2[] = "vgfo";
+	char str3[] = "/frvirejvire";
+	char str4[] = "+";
+	//When
+	const int result1 = CheckDivision(str1);
+	const int result2 = CheckDivision(str2);
+	const int result3= CheckDivision(str3);
+	const int result4= CheckDivision(str4);
+	//Then
+	const int exp1 = 1;
+	const int exp2 = 0;
+	const int exp3 = -1;
+	const int exp4 = 0;
+
+	ASSERT_EQUAL(exp1, result1);
+	ASSERT_EQUAL(exp2, result2);
+	ASSERT_EQUAL(exp3, result3);
+	ASSERT_EQUAL(exp4, result4);
+}
+
+CTEST(menu, check_exponent)
+{
+	//Given
+	char str1[] = "^";
+	char str2[] = "vgfo";
+	char str3[] = "^frvirejvire";
+	char str4[] = "+";
+	//When
+	const int result1 = CheckExponent(str1);
+	const int result2 = CheckExponent(str2);
+	const int result3= CheckExponent(str3);
+	const int result4= CheckExponent(str4);
+	//Then
+	const int exp1 = 1;
+	const int exp2 = 0;
+	const int exp3 = -1;
+	const int exp4 = 0;
+
+	ASSERT_EQUAL(exp1, result1);
+	ASSERT_EQUAL(exp2, result2);
+	ASSERT_EQUAL(exp3, result3);
+	ASSERT_EQUAL(exp4, result4);
+}
