b324066eda9b8761ce93af5f9cb20201ff4bee72
Ubuntu <nikar_50@mail.ru>
2018-09-11 03:54:29 -0700

    add main

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include "header.h"
+
+int main () {
+	
+	char o[6];
+	double r;	
+	while (1){
+    		printf ("Enter operation: ");
+    		scanf ("%s", o);
+    		if (CheckExit (o)==1) {
+			printf ("Quit\n");
+			break;
+		}
+		r=Calc(o);
+		printf ("Result: %lg\n", r); 
+		printf ("\n");
+		fflush (stdin);
+    	}
+}
